9b05ade536b0e1f0a61b2eca0b659489ddead097
Ewerton Maik <ewertonmsa@hotmail.com>
2021-02-07 19:26:25 -0300

    Update Oracle_12C.sql

--- Oracle_12C.sql
+++ Oracle_12C.sql
@@ -1,2 +1,11 @@
 -- Verificar usuários por CDB
-select username, common, con_id from cdb_users
+select username, common, con_id from cdb_users;
+
+-- Verificar roles
+select role, common from dba_roles;
+
+-- Alterar de conexão 
+alter session set container=st_plug;
+
+--Verificar os PDB's
+select pdb_id, dbid, pdb_name, status from cdb_pdbs order by pdb_id;
