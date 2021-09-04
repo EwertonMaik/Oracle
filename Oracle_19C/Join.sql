934276a6bbe6ef2f3c2946239daaaffde05b004e
Ewerton Maik <ewertonmsa@hotmail.com>
2021-09-03 22:05:22 -0300

    Add files via upload

--- Oracle_19C/Join.sql
+++ Oracle_19C/Join.sql
@@ -7,6 +7,7 @@
 5 - RIGHT OUTER JOIN
 6 - FULL OUTER JOIN
 7 - CROSS JOIN
+8 - JOIN ou INNER JOIN -- A palavra INNER é opcional
 
 -- Utilizando Prefixos Coluna com Nomes de Tabela 
 SELECT
@@ -96,3 +97,65 @@ SELECT
 FROM employees empregado
 JOIN employees gerente ON (empregado.manager_id = gerente.employee_id)
 ORDER BY empregado.employee_id;
+
+8 - JOIN ou INNER JOIN -- A palavra INNER é opcional
+-- Join com a Cláusula ON
+SELECT
+		e.employee_id,
+		j.job_title,
+		d.department_name,
+		l.city,
+		l.state_province,
+		l.country_id
+FROM   employees e
+INNER JOIN jobs        j ON e.job_id        = j.job_id
+INNER JOIN departments d ON d.department_id = e.department_id
+INNER JOIN locations   l ON d.location_id   = l.location_id
+ORDER BY e.employee_id;
+
+SELECT
+		e.employee_id,
+		j.job_title,
+		d.department_name,
+		l.city,
+		l.state_province,
+		l.country_id
+FROM   employees e
+JOIN jobs        j ON e.job_id        = j.job_id
+JOIN departments d ON d.department_id = e.department_id
+JOIN locations   l ON d.location_id   = l.location_id
+ORDER BY e.employee_id;
+
+-- Utilizando Natural Joins
+SELECT
+		department_id,
+		department_name,
+		location_id, city
+FROM    departments
+NATURAL INNER JOIN locations;
+
+SELECT
+		department_id,
+		department_name,
+		location_id, city
+FROM    departments
+NATURAL JOIN locations;
+
+-- JOIN com a Cláusula USING
+SELECT
+		e.employee_id,
+		e.last_name,
+		d.location_id,
+		department_id,
+		d.department_name
+FROM employees e
+INNER JOIN departments d USING (department_id);
+  
+SELECT
+		e.employee_id,
+		e.last_name,
+		d.location_id,
+		department_id,
+		d.department_name
+FROM employees e
+JOIN departments d USING (department_id);
