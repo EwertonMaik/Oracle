17b9b754dea52fc8111bb1e9621c9d29d4ee8275
Ewerton Maik <ewertonmsa@hotmail.com>
2023-01-04 06:47:58 -0300

    Update PL SQL.sql

--- PL SQL.sql
+++ PL SQL.sql
@@ -1,6 +1,11 @@
 -- Documentação Scripts - PL/SQL do Banco de Dados ORACLE
+-- Script Estudados do Livro - PL/SQL - Domine a linguagem do banco de dados Oracle
 
--- Estrutura do Bloco
+-- Estrutura do Bloco Anônimo - Não Possui Cabeçalho
+declare
 begin
-..
+  begin
+  end;
+  exception
+  when others then
 end;
