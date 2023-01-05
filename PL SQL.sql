08708998b89a7baee62910fb1e7c63e047cc8568
Ewerton Maik <ewertonmsa@hotmail.com>
2023-01-04 22:44:45 -0300

    Update PL SQL.sql

--- PL SQL.sql
+++ PL SQL.sql
@@ -9,3 +9,44 @@ begin
   exception
   when others then
 end;
+
+
+--## Exemplo 01
+declare
+  soma number;
+begin
+  soma := 45 + 55;
+  dbms_output.put_line('Soma :' || soma);
+exception
+  when others then
+  raise_application_error(-20001, 'Erro ao somar valores!');
+end;
+/ -- Barra irá permitir que o bloco seja executado
+
+--## Exemplo 02
+set serveroutput on -- Comando para permitir que o pacote dbms_output imprima em tela.
+declare
+cursosr c1(pdname varchar2, pmgr number) is
+  select ename, job, dname from emp, dept
+  where emp.deptno = dept.deptno
+  and dept.loc = pdname
+  and emp.mgr = pmgr;
+
+r1 c1%rowtype;
+begin
+  open c1( pmgr => 7698, pdname => 'CHICAGO' );
+  loop
+    fetch c1 into r1;
+    
+    if c1%found then
+      dbms_output.put_line('Nome: ' || r1.ename || 'Cargo: ' || r1.job);
+    else
+    exit;
+    end if;
+    end loop;
+    
+    close c1;
+    exception when others then
+      dbms_output.put_line('Erro: ' || sqlerrm );
+    end;
+    /
